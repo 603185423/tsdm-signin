e5a2cbded20cd6825e31c718ab73829ad8556acf
603185423 <50170900+603185423@users.noreply.github.com>
2024-07-28 13:57:39 +0800

    add param `ping` for uptime-Kuma

--- tsdm_signin.py
+++ tsdm_signin.py
@@ -19,7 +19,7 @@ import requests
 
 from Utils.config import ConfigManager, write_plugin_data
 from Utils.logger import log
-from Utils.notify import send_notification, beat_once, ping_website
+from Utils.notify import send_notification, beat_once
 
 config = ConfigManager().data_obj
 
